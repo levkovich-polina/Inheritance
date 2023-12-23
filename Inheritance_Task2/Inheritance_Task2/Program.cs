d58e9e261d121734242c7686ac3387baad930660
levkovich-polina <levkovich.polina@gmail.com>
2023-12-23 11:44:56 +0300

    Task 2 and 3

---
+++ Inheritance_Task2/Inheritance_Task2/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_Task2
+{
+    public class Book
+    {
+        private string title;
+        private string author;
+        private double price;
+
+        public Book(string title, string author, double price)
+        {
+            this.title = title;
+            this.author = author;
+            this.price = price;
+        }
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        public string Author
+        {
+            get { return author; }
+            set { author = value; }
+        }
+
+        public double Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Название книги: {title}");
+            Console.WriteLine($"Автор: {author}");
+            Console.WriteLine($"Стоимость: {price}руб.");
+        }
+    }
+
+    public class BookGenre : Book
+    {
+        private string genre;
+
+        public BookGenre(string title, string author, double price, string genre)
+            : base(title, author, price)
+        {
+            this.genre = genre;
+        }
+
+        public string Genre
+        {
+            get { return genre; }
+            set { genre = value; }
+        }
+
+        public new void Print()
+        {
+            base.Print();
+            Console.WriteLine($"Жанр: {genre}");
+        }
+    }
+
+    sealed public class BookGenrePubl : BookGenre
+    {
+        private string publisher;
+
+        public BookGenrePubl(string title, string author, double price, string genre, string publisher)
+            : base(title, author, price, genre)
+        {
+            this.publisher = publisher;
+        }
+
+        public string Publisher
+        {
+            get { return publisher; }
+            set { publisher = value; }
+        }
+
+        public new void Print()
+        {
+            base.Print();
+            Console.WriteLine($"Издательство: {publisher}");
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BookGenrePubl book = new BookGenrePubl("Война и мир", "Л.Н. Толстой", 300, "Роман", "Эксмо");
+            book.Print();
+            Console.WriteLine();
+            BookGenrePubl book1= new BookGenrePubl("Колобок", "А.Н. Толстой", 20, "Народная сказка", "Эксмо");
+            book1.Print();
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ Inheritance_Task3/Inheritance_Task3/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_Task3
+{
+    public abstract class Figure
+    {
+        private string name;
+
+        public Figure(string name)
+        {
+            this.name = name;
+        }
+
+        public string Name
+        {
+            get { return name; }
+        }
+
+        public abstract double Area2 { get; }
+        public abstract double Area();
+        public virtual void Print()
+        {
+            Console.WriteLine($"Фигура: {Name}");
+        }
+    }
+
+    public class Triangle : Figure
+    {
+        private double a;
+        private double b;
+        private double c;
+
+        public Triangle(string name, double a, double b, double c) : base(name)
+        {
+            SetABC(a, b, c);
+        }
+
+        public void SetABC(double a, double b, double c)
+        {
+            if (a + b > c && a + c > b && b + c > a)
+            {
+                this.a = a;
+                this.b = b;
+                this.c = c;
+            }
+            else
+            {
+                Console.WriteLine("Некорректные значения сторон треугольника");
+            }
+        }
+
+        public double GetA()
+        {
+            return a;
+        }
+
+        public double GetB()
+        {
+            return b;
+        }
+
+        public double GetC()
+        {
+            return c;
+        }
+
+        public override double Area2
+        {
+            get { return Area(); }
+        }
+
+        public override double Area()
+        {
+            double p = (a + b + c) / 2;
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+
+        public override void Print()
+        {
+            base.Print();
+            Console.WriteLine($"Стороны треугольника: {a}, {b}, {c}");
+        }
+    }
+
+    public class TriangleColor : Triangle
+    {
+        private string color;
+
+        public TriangleColor(string name, double a, double b, double c, string color) : base(name, a, b, c)
+        {
+            this.color = color;
+        }
+
+        public string Color
+        {
+            get { return color; }
+        }
+
+        public override double Area2
+        {
+            get { return base.Area2; }
+        }
+
+        public override double Area()
+        {
+            return base.Area();
+        }
+
+        public override void Print()
+        {
+            base.Print();
+            Console.WriteLine($"Цвет треугольника: {color}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TriangleColor triangle = new TriangleColor("Треугольник", 3, 4, 5, "Красный");
+            triangle.Print();
+            Console.WriteLine($"Площадь треугольника: {triangle.Area2}");
+            Console.WriteLine();
+            TriangleColor tr = new TriangleColor("Треугольник", 6, 8, 7, "Синий");
+            tr.Print();
+            Console.WriteLine($"Площадь треугольника: {tr.Area2}");
+            Console.ReadLine();
+        }
+    }
+}
